308be8c6554302ed55bb5042f434cb118dfde9eb
Arunava Basu <arunavabasudev@gmail.com>
2024-03-15 20:23:14 +0530

    feat: adding some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["fakestoreapi.com", "groww.in"],
+    // domains: ["fakestoreapi.com", "groww.in"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**",
+      },
+    ],
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Groww",
   description: "Assignment by arunavabasu.com",
+  icons: {
+    icon: "/assets/icon.webp",
+  },
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/x-icon" href="/assets/icon.webp"></link>
       <body className={inter.className}>{children}</body>
     </html>
   );
